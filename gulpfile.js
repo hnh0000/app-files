2ef346fd6471e24c300c3625f421eae1888edb61
洪乃火 <www@hnh117.com>
2018-10-25 18:33:30 +0800

    Gulp练习

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,42 @@
-const gulp = require('gulp');
-const cssScss = require('gulp-css-scss');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var copy = require('gulp-contrib-copy');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
 
-gulp.task('css-scss', function() {
-    return gulp.src('./asset/css/*.scss')
-        .pipe(cssScss())
-        .pipe(gulp.dest('./scss/a.css'));
+var jqueryWeui = './node_modules/jquery-weui/**/*';
+var fontAwesome = './node_modules/font-awesome/**/*';
+
+var dist = './asset/',
+    cssSrc = './asset/sass/*',
+    cssDist = './asset/css/',
+    libDist = './asset/lib/';
+
+gulp.task('css', function() {
+    return gulp.src(cssSrc)
+        .pipe(sass())
+        .pipe(gulp.dest(cssDist));
 });
 
-gulp.task('default', ['css-scss']);
+gulp.task('copy', function() {
+    gulp.src(fontAwesome)
+        .pipe(gulp.dest(libDist+'font-awesome'));
+    return gulp.src(jqueryWeui)
+        .pipe(gulp.dest(libDist+'jquery-weui'));
+});
+
+// 自动刷新
+gulp.task('server', function() {
+    browserSync.init({
+        // 指定代理url
+        proxy: "http://app.test:802/files",
+        // 刷新有提示
+        notify: false,
+    });
+    gulp.watch(cssSrc,['css']).on('change', reload);
+    gulp.watch(dist+'js/*.js').on('change', reload);
+    gulp.watch(['index.html', 'html/*.html']).on('change', reload);
+});
+
+gulp.task('default', ['css', 'server']);
+gulp.task('build', ['default', 'copy']);
