55967b31ad0ba9ce899f808bc693c945dcfc0a2f
洪乃火 <www@hnh117.com>
2018-10-26 18:59:28 +0800

    文件浏览

---
+++ script/common.js
@@ -0,0 +1,95 @@
+function fnReady() {
+    fnReadyKeyback();
+    fnReadyOpenWin();
+    fnReadyHeader();
+    fnReadyNav();
+    fnReadyFooter();
+};
+
+// 监听返回键以及类为event-back的元素点击都会closeWin
+function fnReadyKeyback() {
+    var keybacks = $api.domAll('.event-back');
+    for (var i = 0; i < keybacks.length; i++) {
+        $api.attr(keybacks[i], 'tapmode', 'highlight');
+        keybacks[i].onclick = function() {
+            api.closeWin();
+        };
+    }
+
+    api.parseTapmode();
+};
+
+// 点击带有类为open-win的元素会打开新的窗口
+function fnReadyOpenWin() {
+    var buttons = $api.domAll('.open-win');
+    for (var i = 0; i < buttons.length; i++) {
+        $api.attr(buttons[i], 'tapmode', 'highlight');
+        buttons[i].onclick = function() {
+            var target = $api.closest(event.target, '.open-win');
+            var winName = $api.attr(target, 'win'),
+                isNeedLogin = $api.attr(target, 'login'),
+                param = $api.attr(target, 'param');
+
+            if (isNeedLogin && !$api.getStorage('accessToken')) {
+                winName = 'login';
+            }
+
+            if (param) {
+                param = JSON.parse(param);
+            }
+
+            api.openWin({
+                name: winName.replace('html/', ''),
+                url: './' + winName + '.html',
+                pageParam: param
+            });
+        };
+    }
+    api.parseTapmode();
+};
+
+// 顶部元素，以及顶部高度
+var header, headerHeight = 0;
+function fnReadyHeader() {
+    header = $api.byId('header');
+    if (header) {
+        $api.fixIos7Bar(header);
+        headerHeight = $api.offset(header).h;
+    }
+};
+
+// 导航栏以及导航栏高度
+var nav, navHeight = 0;
+function fnReadyNav() {
+    nav = $api.byId('nav');
+    if (nav) {
+        navHeight = $api.offset(nav).h;
+    }
+};
+
+// 底部元素以及底部高度
+var footer, footerHeight = 0;
+function fnReadyFooter() {
+    footer = $api.byId('footer');
+    if (footer) {
+        footerHeight = $api.offset(footer).h;
+    }
+};
+
+// 打开当前窗口对应的Frame
+function fnReadyFrame(winName) {
+    var frameName = winName?winName:(api.winName + '_frame');
+    api.openFrame({
+        name: frameName,
+        url: './' + frameName + '.html',
+        bounces: true,
+        bgColor: '#f0f0f0',
+        rect: {
+            x: 0,
+            y: headerHeight + navHeight,
+            w: 'auto',
+            h: api.winHeight - headerHeight - footerHeight - navHeight
+        },
+        pageParam: api.pageParam
+    });
+};
